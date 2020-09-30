bf9efbe66fd58f8af2e96b0b6d4e857d74c82aff
Ditmar <ditmarhalla@protonmail.ch>
2020-09-30 22:07:55 +0200

    fixed find_class function to give the correct answer for tuple with two variables

--- file.py
+++ file.py
@@ -28,7 +28,7 @@ def find_class(traned_data,  untrained_vector):                         # This f
     for vectors in traned_data:                                         # Loop that will run through all the first data (trained) and second (untrained)
         if len(vectors) == 7:
             distnace = euclidian_distance(vectors[0:5], untrained_vector)  # and calculate the distance using the fuction we created "Euclidian_distance"
-            distnace_vector.append((distnace,tuple(vectors[6])))               # Then adds the "distnace" we found and "class" so that we can use later for comparason
+            distnace_vector.append((distnace,vectors[6]))               # Then adds the "distnace" we found and "class" so that we can use later for comparason
         elif len(vectors) == 6:
             distnace = euclidian_distance(vectors[0:4],untrained_vector)
             distnace_vector.append((distnace, vectors[5]))              ########## distance is type(int) while vector is type(tuple)
@@ -37,7 +37,6 @@ def find_class(traned_data,  untrained_vector):                         # This f
             distnace_vector.append((distnace, vectors[4]))
         else:
             break
-    print(type(distnace_vector[1]))
     min_val = distnace_vector[0]                                        # Create a minimal value to use and find the class of our "untrained_vector". We define it as the first array in the "distance_vector" list
     for i in range(len (distnace_vector)):                              # Run through a loop for how many data we hav in "distnace_vector"
         if min_val[0] > (distnace_vector[i][0]):                        # Compare if the value we have si smaller then the next one
@@ -45,7 +44,9 @@ def find_class(traned_data,  untrained_vector):                         # This f
             #print("minval",min_val)
     #print(type(untrained_vector))
     #print(type(min_val[1]))
-    untrained_vector = untrained_vector + min_val[1]             # After finding the best distance we assigne the class of that distnace to the "untrained_vector"
+    untrained_vector = list(untrained_vector)
+    untrained_vector.append(min_val[1])             # After finding the best distance we assigne the class of that distnace to the "untrained_vector"
+    untrained_vector = tuple(untrained_vector)
     #print ("vectorclass",untrained_vector)
     return untrained_vector
 
@@ -85,14 +86,13 @@ def remove_dimension(data,dimension):                                   # Romves
 
 start = time.time()
 # test run with some data
-number_of_vectors_untrained = 10                                     # We use slices to caluclate only the number we would like
-dataset_trained = file_to_list("trained.txt")                           # Open the untrained data file
-dataset_untrained = file_to_list("untrained.txt")
-print(class_untrained_vectors(dataset_trained,dataset_untrained[0:number_of_vectors_untrained])) # we use slices to call the munber of "untrained" vectors we would like to use
-#print('Time: ', stop - start)
-#trained_data = remove_dimension("trained.txt",2)
-#untrained_data = remove_dimension("untrained.txt",2)
-#print(class_untrained_vectors(trained_data,untrained_data[0:10]))
+number_of_vectors_untrained = 1000                                     # We use slices to caluclate only the number we would like
+#dataset_trained = file_to_list("trained.txt")                           # Open the untrained data file
+#dataset_untrained = file_to_list("untrained.txt")
+#print(class_untrained_vectors(dataset_trained,dataset_untrained[0:number_of_vectors_untrained])) # we use slices to call the munber of "untrained" vectors we would like to use
+trained_data = remove_dimension("trained.txt",2)
+untrained_data = remove_dimension("untrained.txt",2)
+print(class_untrained_vectors(trained_data,untrained_data[0:number_of_vectors_untrained]))
 
 stop = time.time()
 print('Time: ', stop - start)
