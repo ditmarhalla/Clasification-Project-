fc7a5e289fe385d3e9b30d5c3eba4ad63188d422
Ditmar <ditmarhalla@protonmail.ch>
2021-04-16 23:49:18 +0200

    changes

--- project/vector1.py
+++ project/vector1.py
@@ -8,7 +8,9 @@ class Vectors:
         for vector_1, vector_2 in zip(self.tvector,self.uvector): # The loop will run till the length of "vector 1"
             for value_1,value_2 in zip(vector_1,vector_2):
                 value += abs(int(value_1) - int(value_2))
-        return 
+                print ("Value = ", value)
+        return value
+
 
 def file_to_list(name):
     list_of_data = []                                                  # Create empty list to put the data
@@ -25,5 +27,6 @@ udata = 'C:\\Users\\Ditmar\\\Desktop\\University\\2 Semester\\Introduction to Pr
 dataset_trained = file_to_list(tdata)                               # Open the untrained data file
 dataset_untrained = file_to_list(udata)
 
-vector = Vectors(dataset_trained[:10],dataset_untrained[:10])
+for i in range (10):
+    vector = Vectors(dataset_trained[i],dataset_untrained[i])
 print("This is the distance: ",vector.euclidian_distance())
